58b7abeedbce0812e60d5c4b471fe35802ffeef1
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-06-02 18:11:58 -0700

    created a create recipe form

--- client/src/screens/CreateRecipes.jsx
+++ client/src/screens/CreateRecipes.jsx
@@ -1,14 +1,120 @@
-import React from 'react';
+import React, { useState } from "react";
+import Axios from "axios";
 
 // components
-import { Header } from '../components/Header';
+import { Header } from "../components/Header";
 
 export const CreateRecipes = () => {
+  // form inputs
+  const [dishName, setDishName] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [instructions, setInstructions] = useState("");
+  // showToast
+  const [toast, setToast] = useState(false);
+
+  // onSubmit
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    // add new line tag to every new line in text to store in db
+    const modifiedIngredients = ingredients.replace(/\n\n\n/g, "#NEWLINE#");
+    const modifiedInstructions = instructions.replace(/\n\n\n/g, "#NEWLINE#");
+
+    // POST the user input into the recipe-app db
+    try {
+      await Axios.post("http://localhost:5000/recipes", {
+        dishName: dishName,
+        ingredients: modifiedIngredients,
+        instructions: modifiedInstructions,
+      });
+      setToast(true);
+    } catch (error) {
+      console.log(error);
+    }
+
+    // reset the states
+    setDishName("");
+    setIngredients("");
+    setInstructions("");
+  };
+
   return (
     <div>
-        <Header />
-        
+      <Header />
+
+      <section className="container d-flex justify-content-center align-items-center flex-column m-5">
         <h1>Create Recipes</h1>
+
+        <div className="d-flex flex-column card p-5">
+          <form onSubmit={onSubmit}>
+            <div className="input-group m-3">
+              <label className="me-4 form-label" htmlFor="dishName">
+                Dish Name
+              </label>
+              <input
+                type="text"
+                id="dishName"
+                className="form-control"
+                value={dishName}
+                onChange={(e) => setDishName(e.target.value)}
+              />
+            </div>
+            <div className="input-group m-3">
+              <label className="me-4 form-label" htmlFor="ingredients">
+                Ingredients
+              </label>
+              <textarea
+                name="ingredients"
+                id="ingredients"
+                cols="60"
+                rows="10"
+                className="form-control"
+                value={ingredients}
+                onChange={(e) => setIngredients(e.target.value)}
+              ></textarea>
+            </div>
+            <div className="input-group m-3">
+              <label className="me-4 form-label" htmlFor="instruction">
+                Instructions
+              </label>
+              <textarea
+                name="instruction"
+                id="instruction"
+                cols="60"
+                rows="10"
+                className="form-control"
+                value={instructions}
+                onChange={(e) => setInstructions(e.target.value)}
+              ></textarea>
+            </div>
+
+            <div className="d-flex justify-content-center align-items-center">
+              <button className="btn btn-dark btn-block btn-lg" type="submit">
+                Create
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <div
+          class="toast"
+          role="alert"
+          aria-live="assertive"
+          aria-atomic="true"
+          auto-hide='false'
+        >
+          <div class="toast-header">
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="toast"
+              aria-label="Close"
+              onClick={() => setToast(false)}
+            ></button>
+          </div>
+          <div class="toast-body">Hello, world! This is a toast message.</div>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
